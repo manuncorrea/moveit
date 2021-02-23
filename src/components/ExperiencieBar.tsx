36725b2e8a235abe246acfa43fd41b4649c5e8e0
Emanuele Correa <manuncorrea@hotmail.com>
2021-02-23 15:06:13 -0300

    adding NextJs to the application, and starting the hooks

--- src/components/ExperiencieBar.tsx
+++ src/components/ExperiencieBar.tsx
@@ -1,12 +1,12 @@
-import './styles.css';
+import styles from '../styles/components/ExperiencieBar.module.css';
 
 export function ExperienceBar() {
   return(
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: '50%' }}/>
-        <span className="current-experience" style={{ left: '50%'}}>
+        <span className={styles.currentExperience} style={{ left: '50%'}}>
           300 xp
         </span>
       </div>
